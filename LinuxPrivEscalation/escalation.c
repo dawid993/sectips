0330337a3147365abf256929ddb775bc055eeeb6
dawid993 <bieniekdawid2aizst@gmail.com>
2024-09-11 07:48:08 +0200

    Escalation in linux using suid bit + comments

--- LinuxPrivEscalation/escalation.c
+++ LinuxPrivEscalation/escalation.c
@@ -2,6 +2,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/*
+* If executable of that program has suid bit set
+* we can use that to exploit privilages escalation
+* Any binary that has SUID bit set and calling another program from the PATH environment variable is a clear indication of privilege escalation. 
+* All you can do is simply prepend your path variable with the path where your malicious executable exists and it will give you a root shell.
+*/
 int main() {
     uid_t uid = getuid();   // Get the real user ID
     uid_t euid = geteuid(); // Get the effective user ID
